7425c896f45c47138d4b80e16441602c8bed42ee
MindsMirrors <michaeljtrujillo92@gmail.com>
2025-09-28 11:33:30 -0600

    Revert "Updates"
    This reverts commit 9fda6072d6f0ca356a7fec26b584ee33a225d07a.

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router} from "react-router-dom";
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faGhost, faCircleHalfStroke, faBars, faXmark, faMagnifyingGlass, faSpinner, faWindowMaximize, faPlay } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faGhost, faCircleHalfStroke, faBars, faXmark, faMagnifyingGlass, faSpinner, faWindowMaximize, faPlay)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Router basename="/get-good-games-app">
+        <App />
+    </Router>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
